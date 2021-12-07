32754d02fe665d507872deb1bc54b80c2339ad0d
LeanFly <ningmuyu@live.com>
2021-12-07 22:13:09 +0800

    Update tailscale.lua

--- luasrc/controller/tailscale.lua
+++ luasrc/controller/tailscale.lua
@@ -22,4 +22,4 @@ end
 function tail_up()
   local info = luci.sys.exec("tailscale")
   local infoup = luci.sys.exec("tailscale up")
-  m.Map("up", translate("uplink"), <br> .. infoup)
+  m.Map("up", translate("uplink"),translate("uplink") .. "<br>info: <a href=' " .. infoup .. ": '>" .. infoup)
