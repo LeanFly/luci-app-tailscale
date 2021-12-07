81f60bca99fc0405282f42346d4481a71b919c80
LeanFly <ningmuyu@live.com>
2021-12-07 22:27:12 +0800

    Update tailscale.lua

--- luasrc/controller/tailscale.lua
+++ luasrc/controller/tailscale.lua
@@ -7,16 +7,8 @@ function index()
 
   entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
   entry({"admin","vpn", "tailscale"}, firstchild(), _("Tailscale")).dependent = false
-  entry({"admin", "vpn", "tailscale", "status"}, form("tailscale/status"), _("Tailscale Status") , 1, call("tail_up"))
+  entry({"admin", "vpn", "tailscale", "up"}, 1, call("tail_up"))
 
-  entry({"admin","vpn","tailscale","status"},call("act_status"))
-end
-
-function act_status()
-local e={}
-  e.running=luci.sys.call("tailscale up")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
 end
 
 function tail_up()
