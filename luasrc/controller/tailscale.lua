69d834149e546cf84f07b61bacba275dbbcd7060
leanfly <leanwrt@leanwrt.local>
2021-12-08 11:27:19 +0800

    update

--- luasrc/controller/tailscale.lua
+++ luasrc/controller/tailscale.lua
@@ -1,16 +1,8 @@
 module("luci.controller.tailscale",package.seeall)
 
 function index()
-
-  entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
-  entry({"admin", "vpn", "taiscale"}, cbi("tailscale"), _("Tailscale穿透"))
-  -- entry({"admin","vpn", "Tailscale"}, firstchild(), _("Tailscale")).dependent = false
+  entry({"admin", "vpn", "taiscale"}, alias("admin", "vpn", "taiscale","tailscale"), _("Tailscale穿透"), 45)
+  entry({"admin","vpn", "tailscale"}, firstchild(), _("Tailscale")).dependent = false
   -- entry({"admin", "vpn", "tailscale"}, call("tail_up"))
 
 end
-
--- function tail_up()
---   local info = luci.sys.exec("tailscale")
---   local infoup = luci.sys.exec("tailscale up")
---   m.Map("tailscale", translate("uplink"),translate("uplink") .. "<br>info: <a href=' " .. infoup .. ": '>" .. infoup)
--- end
